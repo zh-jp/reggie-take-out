f3b3f82a4e44c21f1c18aaccabbf9ab030a75665
vv <zh_jp2022@outlook.com>
2023-01-13 11:06:21 +0800

    Initial commit

---
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -0,0 +1,13 @@
+package com.example.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class);
+        log.info("项目启动成功。。。");
+    }
+}
