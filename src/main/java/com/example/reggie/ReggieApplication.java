702f10924ad3bfd541c0449112b249ed67061d18
zh-jp <zh_jp2022@outlook.com>
2023-01-13 15:33:30 +0800

    设置service、controller、mapper

--- src/main/java/com/example/reggie/ReggieApplication.java
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -3,7 +3,7 @@ package com.example.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@Slf4j
+@Slf4j //输出日志
 @SpringBootApplication
 public class ReggieApplication {
     public static void main(String[] args) {
